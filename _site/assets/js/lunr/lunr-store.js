d23bf568a051bcd13186ae9a31836089b76af659
borismarinov96 <boris.marinov96@gmail.com>
2021-08-28 20:26:21 +1200

    adding art

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Foo Bar Identity",
+        "excerpt":"                                                                                                                              This is a sample gallery to go along with this case study.      ","categories": [],
+        "tags": [],
+        "url": "/portfolio/foo-bar-website/",
+        "teaser": "/assets/images/gronx-01.png"
+      },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
