896220005628e66a12c6427897577dc0c7506cec
borismarinov96 <boris.marinov96@gmail.com>
2021-08-29 12:32:41 +1200

    first collection

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Dragonfly Guide Illustrations",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/dragonfly-illustrations/",
+        "teaser": "/assets/images/dragon_breasts.JPG"
+      },{
         "title": "Foo Bar Identity",
         "excerpt":"                                                                                                                              This is a sample gallery to go along with this case study.      ","categories": [],
         "tags": [],
